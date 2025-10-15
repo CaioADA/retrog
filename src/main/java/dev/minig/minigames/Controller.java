9100b176be8d9e0828feba1e0d6c4db5542c41d7
CaioADA <mcaioadolfo@gmail.com>
2025-10-15 16:49:41 -0300

    adding html,css,js

--- src/main/java/dev/minig/minigames/Controller.java
+++ src/main/java/dev/minig/minigames/Controller.java
@@ -1,10 +1,9 @@
 package dev.minig.minigames;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller //annotation to define a controller
-public class HomeController {
+@org.springframework.stereotype.Controller //annotation to define a controller
+public class Controller {
     @RequestMapping("/") //annotation to define the route, when the user goes to the root route, this method will be called
     public String index () {
         return "index.html"; //return the name of the view (index.html)
